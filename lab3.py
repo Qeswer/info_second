b544d91438a339718c102e7221bdb31ab0cd693b
Qeswer <56409028+Qeswer@users.noreply.github.com>
2020-03-14 14:00:28 +0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,7 +1,10 @@
+import re
 def first(text):
     print("дан текст: ", text)
-    print("разбиение: ",text.split())
-    Broke=text.split()
+    reg = re.compile('[^a-zA-Z ]')
+    text1 = reg.sub('', text)
+    print(text1)
+    Broke=text1.split()
     k=len(Broke)
     list_three=[]
     for i in range(k):
@@ -9,14 +12,19 @@ def first(text):
             list_three.append(Broke[i])
     print("слова содержащие 3 буквы: "," ".join(list_three))
 first(input(str("введите текст: ")))
+
 def second(text_a):
     print("дан текст: ", text_a)
+    reg = re.compile('[^a-zA-Z ]')
+    text_a = reg.sub('', text_a)
     words = text_a.split()
     new = []
     for i in range(len(words)):
         word=words[i][::-1]
         new.append(word)
+    new.append(str('.'))
     print("ответ:",'\n', ' '.join(new))
 second(str(input("введите зашифрованный текст: ")))
 input()
+
 https://sun9-52.userapi.com/c857420/v857420639/194e26/alcimkjlyjU.jpg
