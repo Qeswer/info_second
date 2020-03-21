04e40302044895cd043534374dd696d7fc731452
Qeswer <56409028+Qeswer@users.noreply.github.com>
2020-03-21 14:11:50 +0500

    Update lab4_1.py

--- lab4/lab4_1.py
+++ lab4/lab4_1.py
@@ -1,27 +1,28 @@
-import  random
-f = open("qeswer.txt","wt")
-numbers = [ random.randint(-10,10) for i in range(int(input("введите N ")))]
+import  random #импортируем библиотеку 
+f = open("qeswer.txt","wt")  #открываем файл, символ "W" означает редактирование, если файл не существует, то он будет 
+numbers = [ random.randint(-10,10) for i in range(int(input("введите N ")))]#создаем список из случайных чисел
 print(numbers)
 for i in range(len(numbers)):
-    f.write(str(numbers[i]) + str("; "))
+    f.write(str(numbers[i]) + str("; ")) #записываем числа в файл
 k = 0
-minus = []
-for i in numbers:
-    for j in numbers:
-        if i!=0 and j!=0 and i==-j:
+minus = [] #создаем список
+for i in numbers: #пробегаемся по списку чисел
+    for j in numbers: #пробегаемся по списку
+        if i!=0 and j!=0 and i==-j: #задаем условие, находим противоположное
             k+=1
-            minus.append(i); minus.append(j) 
-            numbers.remove(i); numbers.remove(j) 
-            break
-for i in range(0,len(minus),2):
-    print("числа:",minus[i],":",minus[i+1])
+            minus.append(i); minus.append(j) #добавляем в новый список числа
+            numbers.remove(i); numbers.remove(j) #удаляем со старого списка противоположные числа
+            break #завершаем
+for i in range(0,len(minus),2): #пробегаемся по четным элементам в новом списке
+    print("числа:",minus[i],":",minus[i+1]) #вывод на экран
     f.write(str("\n"))
     f.write(str("числа: "))
     f.write(str(minus[i]))
     f.write(str(":"))
-    f.write(str(minus[i+1]))
-print("кол-во пар противопол-х чисел:", k)
+    f.write(str(minus[i+1])) #записываем в файл
+print("кол-во пар противопол-х чисел:", k) 
 f.write(str("\n"))
 f.write(str("кол-во пар противопол-х чисел:"))
 f.write(str(k))
-f.close()
+k = int(input())
+f.close() 
