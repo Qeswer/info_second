828ca97f7ac777fe6b2d6998df4ba4419e4eae61
Qeswer <56409028+Qeswer@users.noreply.github.com>
2020-03-21 14:22:50 +0500

    Update lab4_2.py

--- lab4/lab4_2.py
+++ lab4/lab4_2.py
@@ -1,21 +1,21 @@
 import  random
-f = open("qeswer(1).txt","w+")
+f = open("qeswer(1).txt","w+") #Открываем файл, если не существует, создаем
 f1 = open("qeswer(2).txt","w+")
-numbers = [ random.randint(-10,10) for i in range(int(input("введите N ")))]
-k = int(input("введите кратное число k: "))
+numbers = [ random.randint(-10,10) for i in range(int(input("введите N ")))]#рандомный список
+k = int(input("введите кратное число k: ")) #вводим любое число
 print(numbers)
 for i in numbers:
-    f.write(str(i) + str(";"))
-new = []
-for i in numbers:
-    if i%k == 0:
-        new.append(i)
-for i in new:
-    numbers.remove(i)
-for i in numbers:
-    f1.write(str(i) + str(";"))
+    f.write(str(i) + str(";")) #записываем в файл числа
+new = [] #создаем список
+for i in numbers: #пробегаемся по списку чисел
+    if i%k == 0: #условие кратности
+        new.append(i) #добавляем в новый список кратные числа
+for i in new: #пробегаемся по новому списку чисел
+    numbers.remove(i) #удаляем кратные числа со старого списка
+for i in numbers: #пробегаемся по списку, где удалены числа 
+    f1.write(str(i) + str(";")) #записываем в новый файл оставшиеся числа
 f1.write(str("\nудалены элементы: "))
 for i in new:
     f1.write(str(i) + str(";"))
-f.close()
+f.close() #закрытие файла
 f1.close()
